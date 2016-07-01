76fd37be82f84a7ddf46793aaad8b750e4d22616
Vito-Wang <584883060@qq.com>
2016-07-01 15:10:14 +0800

    增加了写入文件功能，为写入数据库做准备

--- 0001/create_cdkey.py
+++ 0001/create_cdkey.py
@@ -1,26 +1,28 @@
 # -*- coding:utf-8 -*-
-# 第0001题，生成200个激活码，激活码由26个大写字母和10个数字组合生成，长度为12位，不能重复，激活码中的字符可以重复
+# 第0001题，生成200个激活码，激活码由26个大写字母和10个数字组合生成，长度为12位，此版本激活码不重复，激活码中的字符可以重复
 # 生成器的版本
 
 import random
 import string
 
 
-def create_cdkey(num, lenth):
+def create_cdkey(num, length):
     """
     num: the cdkeys number
-    lenth: the cdkey lenth
+    length: the cdkey length
     :param num:
-    :param lenth:
+    :param length:
     :return:
     """
+    # 生成由26个大写字母和10个数字组成的列表，ascii_uppercase代表string中的大写字母。
     cdkey_collect = list(string.ascii_uppercase)
     for i in range(0, 10):
         cdkey_collect.append(str(i))
     result = {}
+    # 生成num个激活码，激活码长度为length
     while len(result) < num:
         key = ''
-        for index in range(lenth):
+        for index in range(length):
                 key += (random.choice(cdkey_collect))
         if key in result:
             pass
@@ -33,7 +35,11 @@ def create_cdkey(num, lenth):
 
 
 if __name__ == '__main__':
-    for k in create_cdkey(200, 12):
-        print k
+    try:
+        with open('E:/data1.txt', 'wb') as f:
+            for k in create_cdkey(200, 12):
+                f.write(k + '\n')
 
+    except IOError, e:
+        print 'IOError', e
 
